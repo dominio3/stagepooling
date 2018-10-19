3be38c043e3ad8b68139837313f5e753303392d7
Gamez <Jorge.Gamez@cevalogistics.com>
2018-10-19 12:44:21 -0300

    <?php if (Empty(->photo)): ?> en profile.blade.php

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2,17 +2,24 @@
 
 @section('content')
 
-
+<br>
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
-            <img src="./uploads/photo/{{ $user->photo }}" style="width:150px; height:150px; float:left; border-radius:50%; margin-right:25px;">
+            <?php if (Empty($user->photo)): ?>
+                <img src="./uploads/photo/default.jpg" style="width:150px; height:150px; float:left; border-radius:50%; margin-right:25px;">
+            <?php else: ?>
+                  <img src="./uploads/photo/{{ $user->photo }}" style="width:150px; height:150px; float:left; border-radius:50%; margin-right:25px;">
+            <?php endif; ?>
             <h2>{{ $user->name }}'s Profile</h2>
             <form enctype="multipart/form-data" action="{{ url('/profile') }}" method="POST">
                 <label>Update Profile Image</label>
-                <input type="file" name="photo">
+                <input type="file" name="photo" >
                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                <input type="submit" class="pull-right btn btn-sm btn-primary">
+                <div class="row"></div>
+                <button type="submit" class="pull-right btn btn-lg btn-primary">
+                  <span class="glyphicon glyphicon-arrow-up"></span> Subir
+                </button>
             </form>
         </div>
     </div>
